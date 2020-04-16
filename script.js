3f8a2cee4fed4ddc9eecef58d5c8153e787bb9db
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-04-16 15:01:20 +0200

    første udkast til footer og nav

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.querySelector("nav .menu");
+
+    if (window.innerWidth >= 600) {
+        nav.classList.remove("hide")
+    } else {
+        nav.classList.add("hide")
+    }
+    document.querySelector(".burger_open").addEventListener("click", function () {
+        nav.classList.remove("hide")
+    })
+    document.querySelector(".burger_close").addEventListener("click", function () {
+        nav.classList.add("hide")
+    })
+
+    document.querySelector(".language").addEventListener("click", function () {
+        this.classList.toggle("open");
+    })
+});

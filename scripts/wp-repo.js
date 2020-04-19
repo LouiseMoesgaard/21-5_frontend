ced7591bfad654d7f44fd92a6ba60761e556605f
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-04-19 22:00:12 +0200

    for content ind på flere sider

--- scripts/wp-repo.js
+++ scripts/wp-repo.js
@@ -1,11 +1,10 @@
 const endpoint = "http://moesgaard-designs.dk/kea/09_CMS/21-5/wordpress/wp-json/wp/v2/";
 
-async function setPage(id){
-    if(!id) {
+async function setPage(id) {
+    if (!id) {
         const urlParams = new URLSearchParams(window.location.search);
         id = urlParams.get("id");
     }
-
     const response = await fetch(endpoint + "posts/" + id);
     var post = await response.json();
 

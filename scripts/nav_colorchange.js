5f2c8874c894a5f9d5e2ecf2a50ef813803c0dc3
niko702j <niko702j@stud.kea.dk>
2020-04-23 10:31:25 +0200

    Scoll nav

--- scripts/nav_colorchange.js
+++ scripts/nav_colorchange.js
@@ -1,22 +1,22 @@
 const navigation = document.querySelector('nav');
 window.onscroll = () => {
-    if (this.scrollY <= 20) {
-        changeBack();
-        navigation.classList.remove('scroll');
-    } else {
-        change();
-        navigation.classList.add('scroll');
-    }
+  if (this.scrollY <= 600) {
+    changeBack();
+    navigation.classList.remove('scroll');
+  } else {
+    change();
+    navigation.classList.add('scroll');
+  }
 };
 
 function change() {
-    document.querySelector(".logo img").src = "ikoner/logo_mint.svg";
-    document.querySelector(".login").src = "ikoner/profil_mint.svg";
-    document.querySelector(".burger_open").src = "ikoner/burger_mint.svg";
+  document.querySelector(".logo img").src = "ikoner/logo_mint.svg";
+  document.querySelector(".login").src = "ikoner/profil_mint.svg";
+  document.querySelector(".burger_open").src = "ikoner/burger_mint.svg";
 }
 
 function changeBack() {
-    document.querySelector(".logo img").src = "ikoner/logo_hvid.svg";
-    document.querySelector(".login").src = "ikoner/profil_hvid.svg";
-    document.querySelector(".burger_open").src = "ikoner/burger_hvid.svg";
+  document.querySelector(".logo img").src = "ikoner/logo_hvid.svg";
+  document.querySelector(".login").src = "ikoner/profil_hvid.svg";
+  document.querySelector(".burger_open").src = "ikoner/burger_hvid.svg";
 }
